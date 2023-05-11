8ab888b7427c2a633a578667f93d980c278d479c
Gabriela Vergez <gabyver@hotmail.com>
2023-05-11 18:14:28 -0300

    Agregada app ropa, sus modelos y su primera migracion

---
+++ ropa/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.nombre} , {self.apellido} , {self.email}"
+
+class Prenda(models.Model):
+    nombre = models.CharField(max_length=50)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
