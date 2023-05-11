21c72a5b2e7a7368a549a0c20fb95d8bd0017db0
Gabriela Vergez <gabyver@hotmail.com>
2023-05-11 20:16:12 -0300

    Agregada las views y archivo templates

--- ropa/views.py
+++ ropa/views.py
@@ -1,3 +1,81 @@
 from django.shortcuts import render
 
 # Create your views here.
+def agregar_prendas(request):
+    if request.method == "POST":
+        formulario = PrendaFormulario(request.POST)
+        if formulario.is_valid():
+            data =formulario.cleaned_data
+            nombre = data["nombre"]
+            categoria = data ["categoria"]
+            precio = data ["precio"]
+            descripcion = data ["descripcion"]
+            prenda = Prenda
+            prenda.save()
+            url_exitosa = reverse ("lista_prendas")
+            return redirect (url_exitosa)
+        else:
+            formulario = PrendaFormulario()
+            http_responde = render(
+                request = request,
+                template_name = 'ropa/formulario_prenda.html'
+                context ={'formulario': formulario}
+            )
+            return http_responde
+        
+        
+def buscar_prendas(request):
+    if request.method == "POST":
+        data = request.POST
+        busqueda = data["busqueda"]
+        prendas = Prenda.objects.filter(prenda__icontains=busqueda)
+        contexto = {
+            "prendas": prendas,
+        }
+        http_response= render(
+            request = request,
+            template_name='ropa/busca_clientes.html',
+            context=contexto,
+        )
+        return http_response
+
+
+def listar_prendas(request):
+    contexto = {
+        "prendas": Prenda.objects.all(),
+    }
+    http_response = render(
+        request = request,
+        template_name='ropa/lista_prendas.html',
+        context=contexto,
+    )
+    return http_response
+
+
+def buscar_clientes(request):
+    if request.method == "POST":
+        data = request.POST
+        busqueda = data["busqueda"]
+        clientes = Cliente.objects.filter(cliente__icontains=busqueda)
+        contexto = {
+            "clientes": clientes,
+        }
+        http_response= render(
+            request = request,
+            template_name='ropa/busca_clientes.html',
+            context=contexto,
+        )
+        return http_response
+        
+
+
+def listar_clientes(request):
+    contexto = {
+        "clientes": Cliente.objects.all(),
+    }
+    http_response = render(
+        request = request,
+        template_name='ropa/lista_clientes.html',
+        context=contexto,
+    )
+    return http_response
