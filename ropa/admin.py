024ff2983b396677cdae549e1e3e828eebcfbf5a
Gabriela Vergez <gabyver@hotmail.com>
2023-05-17 10:58:03 -0300

    Importo modelos que tengo que administrar

--- ropa/admin.py
+++ ropa/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from ropa.models import Categoria, Prenda, Cliente
+
+admin.site.register(Categoria)
+admin.site.register(Prenda)
+admin.site.register(Cliente)
